ed4e23a27e6665fe379acbd1b0726aee07af250e
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-02-10 03:49:58 +0200

    fx some the style

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  edit: string = '/assets/icons/edit.png'
   ngOnInit(): void {
   }
 
